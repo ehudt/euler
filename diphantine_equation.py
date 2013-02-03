2c18142830614e0185eed51db191110339976ea8
Ehud Tamir <ehudt71@gmail.com>
2013-02-04 00:33:33 +0200

    updates to problem 66

--- diphantine_equation.py
+++ diphantine_equation.py
@@ -1,8 +1,9 @@
 # problem 66
 
 from math import sqrt
+import gmpy
 
-limit = 10
+limit = 1000
 squares = {x*x : x for x in xrange(limit + 1)}
 Ds = {}
 todo = {d for d in xrange(1, limit + 1) if d not in squares}
@@ -14,18 +15,15 @@ def main():
 	y_sqr = 1
 	while len(todo) > 0:
 		for D in list(todo):
-			#if D in squares or D not in todo:
-			#	continue
-			sqrt_dio = sqrt(D*y_sqr + 1)
-			if sqrt_dio == int(sqrt_dio):
-				Ds[D] = int(sqrt_dio)
+			if gmpy.is_square(D*y_sqr + 1):
+				int_sqrt = int(sqrt(D*y_sqr + 1))
+				Ds[D] = int_sqrt
 				todo.remove(D)
 				print 'D:', D
-				print 'Minimal (x, y):', Ds[D], y
-				if Ds[D] > max_x:
-					max_x = Ds[D]
+				print 'Minimal (x, y):', int_sqrt, y
+				if int_sqrt > max_x:
+					max_x = int_sqrt
 					max_D = D
-				break
 		y_sqr += 2 * y + 1
 		y += 1
 	print max_D
