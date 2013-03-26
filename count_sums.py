99f1f2dc7d4abffbd50045741919c126ae299ba4
Ehud Tamir <ehudt71@gmail.com>
2013-03-26 15:56:09 +0200

    wrong solution

--- count_sums.py
+++ count_sums.py
@@ -1,15 +1,21 @@
 # problem 76
 
-memo = {0 : 1, 1 : 1, }
+from memoize import memoize
 
-def count_sums(n,lim):
-	#if n in memo: 
-	#	return memo[n]
-	if n < 2: 
-		return 1
-	count = 0
-	for i in xrange(n - 1, 0, -1):
-		count += count_sums(i, i)
-	return count
+# for each series length, allow the first number to be no less than n / len(series) 
+# and no more than n + 1 - len(series).
+# length of series can be between 2 and n
+@memoize
+def summations(n, length):
+	if length == 1: return 1
+	sums = 0
+	for first in xrange((n-1)/length + 1, n - length + 2):
+		sums += summations(n - first, length - 1)
+	return sums
 
-print count_sums(5, 4)
+def count_summations(n):
+	if n <= 1: return 0
+	return sum(summations(n, length) for length in xrange(2, n + 1))
+
+if __name__ == '__main__':
+	print count_summations(10)
