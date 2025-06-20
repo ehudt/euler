401c84c61701f97bfaf9283c6d206ebadef9ccb4
Ehud Tamir <1566271+ehudt@users.noreply.github.com>
2025-06-20 11:52:43 +0300

    94: another failed attempt

--- problem94.py
+++ problem94.py
@@ -1,48 +1,81 @@
 #!/usr/bin/env python3
-# encoding: utf-8
 
 from collections import defaultdict, deque
 
+
 def coprime_pairs(limit):
     queue = deque()
-    queue.append((2,1))
+    queue.append((2, 1))
+
     def helper():
         while queue:
             m, n = queue.popleft()
             if 2 * m * (m + n) <= limit:
-                queue.append((2*m-n, m))
-                queue.append((2*m+n, m))
-                queue.append((m+2*n, n))
+                queue.append((2 * m - n, m))
+                queue.append((2 * m + n, m))
+                queue.append((m + 2 * n, n))
                 yield (m, n)
+
     return helper()
+
+
+def vecmul(v, u):
+    return sum(x * y for x, y in zip(v, u))
+
+
+def matmul(A, B):
+    return [[vecmul(A[i], B[j]) for j in range(len(B[0]))] for i in range(len(A))]
+
+
+A = [
+    [1, -2, 2],
+    [2, -1, 2],
+    [2, -2, 3],
+]
+
+B = [
+    [1, 2, 2],
+    [2, 1, 2],
+    [2, 2, 3],
+]
+
+C = [
+    [-1, 2, 2],
+    [-2, 1, 2],
+    [-2, 2, 3],
+]
+
+
+def all_triples():
+    q = deque()
+    q.append([[3, 4, 5]])
+    while q:
+        v = q.popleft()
+        yield v[0]
+        q.extend(
+            [
+                matmul(v, A),
+                matmul(v, B),
+                matmul(v, C),
+            ]
+        )
+
 
 def main():
     print("Problem 94")
-    limit = 1000*1000*1000
-    all_pairs = coprime_pairs(limit)
-    # counter = defaultdict(lambda : 0)
+    limit = 1000 * 1000 * 1000
     s = 0
-    for m, n in all_pairs:
-        k = 1
-        print(m, n)
-        while True:
-            a_ = k*(m*m - n*n)
-            b_ = k*(2*m*n)
-            c = k*(m*m + n*n)
-            a = a_*2 if a_%2==1 else b_*2
-            if not (a+1 == c or a-1 == c): continue
-            if a + c + c > limit: break
-            # counter[a+b+c] += 1
-            s += a+c+c
-            k += 1
-    # total = 0
-    # for i in range(limit + 1):
-    #     if counter[i] == 1:
-    #         total += 1
-    # print(total)
+    for a_, b_, c in all_triples():
+        for k in range(1, limit):
+            a_, b_, c = a_ * k, b_ * k, c * k
+            a = a_ * 2 if a_ % 2 == 1 else b_ * 2
+            if not (a + 1 == c or a - 1 == c):
+                continue
+            if a + c + c > limit:
+                break
+            s += a + c + c
     print(s)
 
-if __name__ == '__main__':
-    main()
 
-    
+if __name__ == "__main__":
+    main()
