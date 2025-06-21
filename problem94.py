399f78e24d6b63b07c3511d5ce1e4f986800abae
Ehud Tamir <1566271+ehudt@users.noreply.github.com>
2025-06-21 12:16:14 +0300

    getting there....??

--- problem94.py
+++ problem94.py
@@ -6,41 +6,22 @@ from gmpy2 import mpz, is_square
 # def is_square(x):
 #     return x == int(x**.5)**2
 
+
 def main():
     print("Problem 94")
     limit = mpz(1000 * 1000 * 1000)
     s = 0
-    x = mpz(1)
+    x = 1
     while x + x + x - 1 <= limit:
         # x - 1 case
-        if is_square(3 * x - 1) and is_square(x + 1) and x + x + x - 1 <= limit:
+        area1 = ((3 * x - 1) * (x + 1)) // 4 * ((x - 1) * (x - 1) // 4)
+        if is_square(area1) and x + x + x - 1 <= limit:
             s += x + x + x - 1
+        area2 = ((3 * x + 1) * (x - 1)) // 4 * ((x + 1) * (x + 1) // 4)
         # x + 1 case
-        if is_square(3 * x + 1) and is_square(x - 1) and x + x + x + 1 <= limit:
+        if is_square(area2) and x + x + x + 1 <= limit:
             s += x + x + x + 1
         x += 2
-    print(s)
-
-
-def main2():
-    print("Problem 94 with NumPy")
-    limit = 1_000_000_000
-    s = 0
-
-    x_vals = np.arange(1, int(limit / 3) + 1)
-    for delta in [-1, 1]:
-        y_vals = x_vals + delta
-        perimeter = 3 * x_vals + delta
-        valid = perimeter <= limit
-
-        x = x_vals[valid]
-        y = y_vals[valid]
-        h2 = x * x - (y * y) // 4
-        y_even = y % 2 == 0
-        h2_four = h2 % 4 == 0
-
-        s += np.sum(perimeter[valid][y_even | h2_four])
-
     print(s)
 
 
